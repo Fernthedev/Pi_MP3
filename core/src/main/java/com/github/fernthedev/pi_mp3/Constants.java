d82f734b8f4323c6da0ad35b7cb3f474d438b1c9
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2020-06-18 08:55:09 -0400

    Initial server code setup.

---
+++ core/src/main/java/com/github/fernthedev/pi_mp3/Constants.java
@@ -0,0 +1,7 @@
+package com.github.fernthedev.pi_mp3;
+
+public class Constants {
+
+    public static int port = 3422;
+
+}
---
+++ core/src/main/java/com/github/fernthedev/pi_mp3/MP3ServerSettings.java
@@ -0,0 +1,10 @@
+package com.github.fernthedev.pi_mp3;
+
+import com.github.fernthedev.lightchat.server.settings.ServerSettings;
+
+public class MP3ServerSettings extends ServerSettings {
+
+    public MP3ServerSettings() {
+        setPort(Constants.port);
+    }
+}
