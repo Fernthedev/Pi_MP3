95c6d5c16a74ea564d902134c114436bd9c330d4
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2020-06-20 15:31:32 -0400

    Audio boilerplate and testing.

--- core/src/main/java/com/github/fernthedev/pi_mp3/core/MP3Server.java
+++ core/src/main/java/com/github/fernthedev/pi_mp3/core/MP3Server.java
@@ -1,5 +1,6 @@
 package com.github.fernthedev.pi_mp3.core;
 
+import com.github.fernthedev.lightchat.core.StaticHandler;
 import com.github.fernthedev.lightchat.server.Server;
 import com.github.fernthedev.lightchat.server.terminal.ServerTerminal;
 import com.github.fernthedev.lightchat.server.terminal.ServerTerminalSettings;
@@ -10,12 +11,25 @@ import com.github.fernthedev.pi_mp3.api.module.ModuleHandler;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import lombok.Getter;
+import org.lwjgl.openal.AL;
+import org.lwjgl.openal.ALC;
+import org.lwjgl.openal.ALCCapabilities;
+import org.lwjgl.openal.ALCapabilities;
+import org.lwjgl.system.MemoryStack;
 
 import java.io.File;
+import java.nio.IntBuffer;
+import java.nio.ShortBuffer;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ExecutorService;
+
+import static org.lwjgl.openal.AL10.*;
+import static org.lwjgl.openal.ALC10.*;
+import static org.lwjgl.stb.STBVorbis.stb_vorbis_decode_filename;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.libc.LibCStdlib.free;
 
 /**
  * Contains and manages the core of the application.
@@ -74,6 +88,108 @@ public class MP3Server extends ServerTerminal implements ICore {
         moduleHandler.scanDirectory(moduleFolder, getClass().getClassLoader());
 
         moduleHandler.initializeModules().awaitFinish(1);
+
+//        ALC.create();
+        server.addShutdownListener(ALC::destroy);
+
+        //Initialization
+        String defaultDeviceName = alcGetString(0, ALC_DEFAULT_DEVICE_SPECIFIER);
+        long   device            = alcOpenDevice(defaultDeviceName);
+
+        int[] attributes = {0};
+        long  context    = alcCreateContext(device, attributes);
+        alcMakeContextCurrent(context);
+
+        ALCCapabilities alcCapabilities = ALC.createCapabilities(device);
+        ALCapabilities  alCapabilities  = AL.createCapabilities(alcCapabilities);
+
+
+
+        if (StaticHandler.isDebug()) {
+
+
+            ShortBuffer rawAudioBuffer;
+
+            int channels;
+            int sampleRate;
+
+            try (MemoryStack stack = stackPush()) {
+                //Allocate space to store return information from the function
+                IntBuffer channelsBuffer   = stack.mallocInt(1);
+                IntBuffer sampleRateBuffer = stack.mallocInt(1);
+
+//                try {
+//                    Resources.getResource("sound.ogg");
+//                } catch (IllegalArgumentException e) {
+//                    throw new RuntimeException(new FileNotFoundException("Cannot find sound.ogg"));
+//                }
+
+                StaticHandler.getCore().getLogger().debug(new File(".").getAbsolutePath() + " is path");
+
+                rawAudioBuffer = stb_vorbis_decode_filename("sound.ogg", channelsBuffer, sampleRateBuffer);
+
+                //Retreive the extra information that was stored in the buffers by the function
+                channels = channelsBuffer.get(0);
+                sampleRate = sampleRateBuffer.get(0);
+            }
+
+//Find the correct OpenAL format
+            int format = -1;
+            if (channels == 1) {
+                format = AL_FORMAT_MONO16;
+            } else if (channels == 2) {
+                format = AL_FORMAT_STEREO16;
+            }
+
+//Request space for the buffer
+            int bufferPointer = alGenBuffers();
+
+//Send the data to OpenAL
+
+            if (rawAudioBuffer == null) throw new NullPointerException("Audio is null for some reason");
+
+            alBufferData(bufferPointer, format, rawAudioBuffer, sampleRate);
+
+//Free the memory allocated by STB
+            free(rawAudioBuffer);
+
+//Request a source
+            int sourcePointer = alGenSources();
+
+//Assign the sound we just loaded to the source
+            alSourcei(sourcePointer, AL_BUFFER, bufferPointer);
+
+//Play the sound
+            alSourcePlay(sourcePointer);
+
+            try {
+
+
+                int bits = alGetBufferi(bufferPointer, AL_BITS);
+                int sizeInBytes = alGetBufferi(bufferPointer, AL_SIZE);
+
+
+                int lengthInSamples = sizeInBytes * 8 / (channels * bits);
+                float samplesInMillis = (lengthInSamples / (float) sampleRate) * 1000;
+
+                StaticHandler.getCore().getLogger().debug("Duration of sound: " + samplesInMillis);
+
+                //Wait for a second
+                Thread.sleep((long) samplesInMillis);
+            } catch (InterruptedException ignored) {
+            }
+
+
+
+            //Terminate OpenAL
+            alDeleteSources(sourcePointer);
+            alDeleteBuffers(bufferPointer);
+            alcDestroyContext(context);
+            alcCloseDevice(device);
+        }
+
+        server.addShutdownListener(ALC::destroy);
+
         started = true;
     }
 
