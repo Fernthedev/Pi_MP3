ac229413b83b32a306e3ce25f79431e95cd61a01
Alex <40070530+DoubleScripts@users.noreply.github.com>
2020-06-20 14:53:34 -0400

    Added default author

--- api/src/test/java/com/github/fernthedev/pi_mp3/core/test/TestModuleSelfClass.java
+++ api/src/test/java/com/github/fernthedev/pi_mp3/core/test/TestModuleSelfClass.java
@@ -3,7 +3,7 @@ package com.github.fernthedev.pi_mp3.core.test;
 import com.github.fernthedev.pi_mp3.api.module.Module;
 import com.github.fernthedev.pi_mp3.api.module.ModuleInfo;
 
-@ModuleInfo(name = "TestModuleSelf", depend = "TestModuleSelf")
+@ModuleInfo(authors = "MP3PiTeam", name = "TestModuleSelf", depend = "TestModuleSelf")
 public class TestModuleSelfClass extends Module {
 
 
